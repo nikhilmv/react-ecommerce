74d7807f6710fd3ac014cca512e5de7ee89db81b
nikhilmv <nikhil.v@iroidtechnologies.com>
2024-06-11 17:50:54 +0530

    changes

--- src/pages/User/UserRegister.js
+++ src/pages/User/UserRegister.js
@@ -1,13 +1,132 @@
 
-
-
+import { useEffect, useState } from 'react';
+import TextField from '@mui/material/TextField'
+import Avatar from '@mui/material/Avatar'
+import FormControlLabel from '@mui/material/FormControlLabel'
+import Radio from '@mui/material/Radio'
+import RadioGroup from '@mui/material/RadioGroup'
+import { useSnackbar } from 'notistack';
+import { Link, useNavigate } from 'react-router-dom';
+ 
 
 export const UserRegister = () => {
 
     return (
-    <h1>register</h1>
+        <>
+         
+            { 
+            <main className="w-full mt-12 sm:pt-20 sm:mt-0">
 
+                {/* <!-- row --> */}
+                <div className="flex md:w-4/6 sm:mt-4 m-auto mb-7 bg-white shadow-lg">
 
+   
+
+                    {/* <!-- signup column --> */}
+                    <div className="flex-1 overflow-hidden">
+
+                        {/* <!-- personal info procedure container --> */}
+                        <form
+                            
+                            encType="multipart/form-data"
+                            className="p-5 sm:p-10"
+                        >
+                            <div className="flex flex-col gap-4 items-start">
+
+                                {/* <!-- input container column --> */}
+                                <div className="flex flex-col w-full justify-between sm:flex-col gap-3 items-center">
+                                    <TextField
+                                        fullWidth
+                                        id="full-name"
+                                        label="Full Name"
+                                        name="name"
+                                   
+                                    
+                                        required
+                                    />
+                                    <TextField
+                                        fullWidth
+                                        id="email"
+                                        label="Email"
+                                        type="email"
+                                        name="email"
+                                 
+                                        required
+                                    />
+                                </div>
+                                {/* <!-- input container column --> */}
+
+                                {/* <!-- gender input --> */}
+                                <div className="flex gap-4 items-center">
+                                    <h2 className="text-md">Your Gender :</h2>
+                                    <div className="flex items-center gap-6" id="radioInput">
+                                        <RadioGroup
+                                            row
+                                            aria-labelledby="radio-buttons-group-label"
+                                            name="radio-buttons-group"
+                                        >
+                                            <FormControlLabel name="gender" value="male"   label="Male" />
+                                            <FormControlLabel name="gender" value="female"   label="Female" />
+                                        </RadioGroup>
+                                    </div>
+                                </div>
+                                {/* <!-- gender input --> */}
+
+                                {/* <!-- input container column --> */}
+                                <div className="flex flex-col w-full justify-between sm:flex-row gap-3 items-center">
+                                    <TextField
+                                        id="password"
+                                        label="Password"
+                                        type="password"
+                                        name="password"
+                         
+                                        required
+                                    />
+                                    <TextField
+                                        id="confirm-password"
+                                        label="Confirm Password"
+                                        type="password"
+                                        name="cpassword"
+                 
+                                        required
+                                    />
+                                </div>
+                                {/* <!-- input container column --> */}
+
+                                <div className="flex flex-col w-full justify-between sm:flex-row gap-3 items-center">
+                                    <Avatar
+                                        alt="Avatar Preview"
+                         
+                                        sx={{ width: 56, height: 56 }}
+                                    />
+                                    <label className="rounded font-medium bg-gray-400 text-center cursor-pointer text-white w-full py-2 px-2.5 shadow hover:shadow-lg">
+                                        <input
+                                            type="file"
+                                            name="avatar"
+                                            accept="image/*"
+                               
+                                            className="hidden"
+                                        />
+                                        Choose File
+                                    </label>
+                                </div>
+                                <button   type="submit" className="text-white py-3 w-full bg-primary-orange shadow hover:shadow-lg rounded-sm font-medium">Signup</button>
+                                <Link to="/login" className="hover:bg-gray-50 text-primary-blue text-center py-3 w-full shadow border rounded-sm font-medium">Existing User? Log in</Link>
+                            </div>
+
+                        </form>
+                        {/* <!-- personal info procedure container --> */}
+
+                    </div>
+                    {/* <!-- signup column --> */}
+            
+                </div>
+                {/* <!-- row --> */}
+
+            </main>
+
+            }
+        </>
     );
 
 };
