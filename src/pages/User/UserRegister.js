70709b4de522acb5af78c4ae24fdfa175a5193ba
nikhilmv <nikhil.v@iroidtechnologies.com>
2024-06-18 22:49:42 +0530

    register

--- src/pages/User/UserRegister.js
+++ src/pages/User/UserRegister.js
@@ -5,11 +5,74 @@ import Avatar from '@mui/material/Avatar'
 import FormControlLabel from '@mui/material/FormControlLabel'
 import Radio from '@mui/material/Radio'
 import RadioGroup from '@mui/material/RadioGroup'
-import { useSnackbar } from 'notistack'; 
+import { useSnackbar } from 'notistack';
 import { Link, useNavigate } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux'; 
+import { useDispatch, useSelector } from 'react-redux';
+ 
 
 export const UserRegister = () => {
+    const dispatch = useDispatch();
+    const { enqueueSnackbar } = useSnackbar();
+
+
+    const [avatar, setAvatar] = useState(); 
+    const [avatarPreview, setAvatarPreview] = useState("preview.png");
+
+    const [user, setUser] = useState({
+        name: "",
+        email: "",
+        gender: "",
+        password: "",
+        cpassword: "",
+    });
+
+    const { name, email, gender, password, cpassword } = user;
+ 
+
+    const handleRegister = (e) => {
+        e.preventDefault();
+        if (password.length < 8) {
+            enqueueSnackbar("Password length must be atleast 8 characters", { variant: "warning" });
+            return;
+        }
+        if (password !== cpassword) {
+            enqueueSnackbar("Password Doesn't Match", { variant: "error" });
+            return;
+        }
+        if (!avatar) {
+            enqueueSnackbar("Select Avatar", { variant: "error" });
+            return;
+        }
+      
+        const formData = new FormData();
+        formData.set("name", name);
+        formData.set("email", email);
+        formData.set("gender", gender);
+        formData.set("password", password);
+        formData.set("avatar", avatar);
+ 
+    }
+
+    const handleDataChange = (e) => {
+        
+        if (e.target.name === "avatar") {
+         
+            const reader = new FileReader(); 
+            reader.onload = () => {
+                if (reader.readyState === 2) {
+                    setAvatarPreview(reader.result);
+                     setAvatar(reader.result); 
+                  
+                }
+            };
+           
+            reader.readAsDataURL(e.target.files[0]);
+           
+        } else {
+             setUser({ ...user, [e.target.name]: e.target.value });
+        }
+    }
+
 
     return (
         <>
@@ -21,13 +84,17 @@ export const UserRegister = () => {
                 <div className="flex md:w-4/6 sm:mt-4 m-auto mb-7 bg-white shadow-lg">
 
    
+                <div className="loginSidebar bg-primary-blue px-9 py-10 hidden sm:flex flex-col gap-4 w-2/5">
+                    <h1 className="font-medium text-white text-3xl">Looks like you're new here!</h1>
+                    <p className="text-gray-200 text-lg pr-2">Sign up with your mobile number to get started</p>
+                </div>
 
                     {/* <!-- signup column --> */}
                     <div className="flex-1 overflow-hidden">
 
                         {/* <!-- personal info procedure container --> */}
                         <form
-                            
+                            onSubmit={handleRegister} 
                             encType="multipart/form-data"
                             className="p-5 sm:p-10"
                         >
@@ -40,8 +107,8 @@ export const UserRegister = () => {
                                         id="full-name"
                                         label="Full Name"
                                         name="name"
-                                   
-                                    
+                                        value={name}
+                                        onChange={handleDataChange} 
                                         required
                                     />
                                     <TextField
@@ -50,14 +117,27 @@ export const UserRegister = () => {
                                         label="Email"
                                         type="email"
                                         name="email"
-                                 
+                                        value={email}
+                                        onChange={handleDataChange}
                                         required
                                     />
                                 </div>
                                 {/* <!-- input container column --> */}
 
                                 {/* <!-- gender input --> */}
-               
+                                <div className="flex gap-4 items-center">
+                                    <h2 className="text-md">Your Gender :</h2>
+                                    <div className="flex items-center gap-6" id="radioInput">
+                                        <RadioGroup
+                                            row
+                                            aria-labelledby="radio-buttons-group-label"
+                                            name="radio-buttons-group"
+                                        >
+                                            <FormControlLabel name="gender" value="male"  onChange={handleDataChange} control={<Radio required />} label="Male" />
+                                            <FormControlLabel name="gender" value="female" onChange={handleDataChange} control={<Radio required />} label="Female" />
+                                        </RadioGroup>
+                                    </div>
+                                </div>
                                 {/* <!-- gender input --> */}
 
                                 {/* <!-- input container column --> */}
@@ -67,7 +147,8 @@ export const UserRegister = () => {
                                         label="Password"
                                         type="password"
                                         name="password"
-                         
+                                        value={password}
+                                        onChange={handleDataChange}
                                         required
                                     />
                                     <TextField
@@ -75,7 +156,8 @@ export const UserRegister = () => {
                                         label="Confirm Password"
                                         type="password"
                                         name="cpassword"
-                 
+                                        value={cpassword}
+                                        onChange={handleDataChange}
                                         required
                                     />
                                 </div>
@@ -84,7 +166,7 @@ export const UserRegister = () => {
                                 <div className="flex flex-col w-full justify-between sm:flex-row gap-3 items-center">
                                     <Avatar
                                         alt="Avatar Preview"
-                         
+                                        src={avatarPreview}
                                         sx={{ width: 56, height: 56 }}
                                     />
                                     <label className="rounded font-medium bg-gray-400 text-center cursor-pointer text-white w-full py-2 px-2.5 shadow hover:shadow-lg">
@@ -92,7 +174,7 @@ export const UserRegister = () => {
                                             type="file"
                                             name="avatar"
                                             accept="image/*"
-                               
+                                            onChange={handleDataChange} 
                                             className="hidden"
                                         />
                                         Choose File
